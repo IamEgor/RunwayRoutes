aaeea1d154603218059ef03a1e898ab195161ea6
Yegor <payforebayby@gmail.com>
2022-07-26 17:21:29 +0300

    add find rounte path utils;

---
+++ app/src/main/java/com/runway/routes/utils/RouteUtils.kt
@@ -0,0 +1,68 @@
+package com.runway.routes.utils
+
+import com.runway.routes.domain.entity.RunwayEntity
+import com.runway.routes.domain.exception.NoPathException
+
+@Throws(NoPathException::class)
+fun findRoute(
+    aircraftRangeKm: Double,
+    firstPoint: RunwayEntity,
+    lastPoint: RunwayEntity,
+    points: List<RunwayEntity>
+): List<RunwayEntity> {
+
+    //points sorted toward the last
+    val sortedPoints = points
+        .map { point -> distanceKm(point, lastPoint) to point }
+        .sortedBy { it.first }
+
+    val routePoints = mutableListOf(firstPoint)
+    val excludePoints = mutableListOf<RunwayEntity>()
+    var currentPoint: RunwayEntity = firstPoint
+
+    while (currentPoint != lastPoint) {
+
+        val nextPoint = findNextPoint(aircraftRangeKm, currentPoint, excludePoints, sortedPoints)
+
+        currentPoint = if (nextPoint == null) {
+            excludePoints.add(routePoints.removeLast())
+            routePoints.lastOrNull() ?: throw NoPathException()
+        } else {
+            nextPoint.also { routePoints.add(it) }
+        }
+    }
+
+    return routePoints
+}
+
+private fun findNextPoint(
+    aircraftRangeKm: Double,
+    prevPoint: RunwayEntity,
+    excludePoints: List<RunwayEntity>,
+    sortedPoints: List<Pair<Double, RunwayEntity>>
+): RunwayEntity? {
+    sortedPoints.forEach { (_, point) ->
+        if (excludePoints.contains(point)) return@forEach
+        val distanceFromPrev = distanceKm(prevPoint, point)
+        if (distanceFromPrev < aircraftRangeKm) {
+            return point
+        }
+    }
+    return null
+}
+
+private fun distanceKm(from: RunwayEntity, to: RunwayEntity): Double {
+    return distanceKm(from.lat, from.lon, to.lat, to.lon)
+}
+
+private fun distanceKm(
+    fromLatitude: Double,
+    fromLongitude: Double,
+    toLatitude: Double,
+    toLongitude: Double
+): Double {
+    return SphericalUtil.distanceRadians(
+        Math.toRadians(fromLatitude), Math.toRadians(fromLongitude),
+        Math.toRadians(toLatitude), Math.toRadians(toLongitude)
+    ) * MathUtil.EARTH_RADIUS / 1000
+}
---
+++ app/src/test/java/com/runway/routes/RouteUtilsTest.kt
@@ -0,0 +1,59 @@
+package com.runway.routes
+
+import com.runway.routes.data.model.PointsResponse
+import com.runway.routes.domain.entity.RunwayEntity
+import com.runway.routes.domain.entity.RunwayType
+import com.runway.routes.utils.findRoute
+import junit.framework.Assert.assertEquals
+import nl.adaptivity.xmlutil.serialization.XML
+import org.junit.Test
+import java.io.File
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+class RouteUtilsTest {
+
+    companion object {
+        const val PLANE_DISTANCE = 300.0
+    }
+
+    @OptIn(ExperimentalTime::class)
+    @Test
+    fun test() {
+        val xml = XML
+        val text = File("src/test/res/aopa-points-export.xml").readText()
+        val points = xml.decodeFromString<PointsResponse>(text).point
+            .map { point ->
+                RunwayEntity(
+                    point.id,
+                    point.id,
+                    point.lat,
+                    point.lon,
+                    point.name,
+                    point.name_ru,
+                    point.active,
+                    point.type,
+                    point.index,
+                    point.index_ru,
+                    point.belongs
+                )
+            }
+            .filter { it.active }
+            .filter { it.belongs != null }
+            .filter { it.type == RunwayType.AIRPORT }
+
+        println("points.size - ${points.size}")
+
+        val firstPoint = points.find { it.indexRu == "ЗЦЕ5" }!!
+        val lastPoint = points.find { it.indexRu == "УННМ" }!!
+
+        val route: List<RunwayEntity>
+        val measureTime = measureTime {
+            route = findRoute(PLANE_DISTANCE, firstPoint, lastPoint, points)
+        }
+
+        println("measureTime - $measureTime")
+
+        assertEquals(route.size, 15)
+    }
+}
