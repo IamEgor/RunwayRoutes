b2d9976216c1a1d6a7a1f7214c96249b75e905e7
Yegor <payforebayby@gmail.com>
2022-08-07 21:57:15 +0300

    hide debug code, add aopa-points-export.xml for debug build;

--- app/src/main/java/com/runway/routes/data/RunwayRemoteDataSource.kt
+++ app/src/main/java/com/runway/routes/data/RunwayRemoteDataSource.kt
@@ -1,12 +1,10 @@
 package com.runway.routes.data
 
-import android.util.Log
-import com.runway.routes.RunwayApp
+import com.runway.routes.BuildConfig
 import com.runway.routes.data.response.PointsResponse
 import io.ktor.client.*
-import nl.adaptivity.xmlutil.serialization.XML
+import io.ktor.client.request.*
 import kotlin.time.ExperimentalTime
-import kotlin.time.measureTime
 
 
 class RunwayRemoteDataSource(private val client: HttpClient) {
@@ -18,34 +16,32 @@ class RunwayRemoteDataSource(private val client: HttpClient) {
     @Deprecated("from network")
     @OptIn(ExperimentalTime::class)
     suspend fun load(): PointsResponse {
-
-        val response: PointsResponse
-        val measureTime = measureTime {
-            val text = RunwayApp.instance
-                .assets
-                .open("aopa-points-export.xml")
-                .bufferedReader().use { it.readText() }
-
-            response = XML.decodeFromString(text)
-        }
-        Log.w(
-            "RunwayRemoteDataSource",
-            "load() measureTime - $measureTime, size - ${response.point.size}, response - $response"
-        )
-//        val clientBuilder: HttpRequestBuilder.() -> Unit = {
-//            url {
-//                path("export/exportFormRequest")
-//                parameters.apply {
-//                    append("exportType", "standart")
-//                    append("exportAll[]", "airport")
-//                    append("exportAll[]", "vert")
-//                    append("exportFormat", "xml")
-//                    append("api_key", BuildConfig.API_KEY)
-//                }
-//            }
+//        val response: PointsResponse
+//        val measureTime = measureTime {
+//            val text = RunwayApp.instance
+//                .assets
+//                .open("aopa-points-export.xml")
+//                .bufferedReader().use { it.readText() }
+//
+//            response = XML.decodeFromString(text)
 //        }
-//        return client.get(HOST, clientBuilder)
-
-        return response
+//        Log.w(
+//            "RunwayRemoteDataSource",
+//            "load() measureTime - $measureTime, size - ${response.point.size}, response - $response"
+//        )
+//        return response
+        val clientBuilder: HttpRequestBuilder.() -> Unit = {
+            url {
+                path("export/exportFormRequest")
+                parameters.apply {
+                    append("exportType", "standart")
+                    append("exportAll[]", "airport")
+                    append("exportAll[]", "vert")
+                    append("exportFormat", "xml")
+                    append("api_key", BuildConfig.API_KEY)
+                }
+            }
+        }
+        return client.get(HOST, clientBuilder)
     }
 }
